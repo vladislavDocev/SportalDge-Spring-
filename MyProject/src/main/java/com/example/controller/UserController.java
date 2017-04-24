5e2d5e53835653f8d7e730d35b58a2e60c28f30a
Tsanko Georgiev <fmi.tsankogeorgiev@gmail.com>
2017-04-24 21:33:00 +0300

    updated userController methods

--- MyProject/src/main/java/com/example/controller/UserController.java
+++ MyProject/src/main/java/com/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -9,18 +11,62 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.example.model.Comment;
+import com.example.model.User;
+import com.example.model.dao.CommentDAO;
+
 @Controller
 @SessionAttributes("user")
 public class UserController {
 
+	
+	@RequestMapping(value="/createComment", method=RequestMethod.GET)
+	public String createComment(Model model) {
+		Comment comment = new Comment();
+		model.addAttribute("comment", comment);
+		return "post";
+	}
+	
 	@RequestMapping(value="/comment", method=RequestMethod.POST)
-	public @ResponseBody String comment(Model model, HttpSession s) {
+	public  String comment(Model model, HttpSession s) {
 		String location = "post";
 		if(!s.isNew()) {
+			//get info from front end
+			try {
+				//make comment
+				
+				Comment c = null;
+				CommentDAO.getInstance().addComment(c);
+			} catch (SQLException e) {
+				//show error page
+				location = "";
+			}
 			location = "post";
 		}else{
 			location = "index";
 		}
 		return location;
 	}
+	
+	@RequestMapping(value="/like", method=RequestMethod.POST)
+	public  String like(Model model, HttpSession s) {
+		String location = "post";
+		if(!s.isNew()) {
+			try {
+				//like comment
+				//get comment from front end
+				Comment c = new Comment();
+				c.like();
+				CommentDAO.getInstance().addComment(c);
+			} catch (SQLException e) {
+				//show error page
+				location = "";
+			}
+			location = "post";
+		}else{
+			location = "index";
+		}
+		return location;
+	}
+	
 }
